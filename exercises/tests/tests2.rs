399d3d28fbdefed88e80f6673963cc3bf53efc95
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-05-22 15:22:45 -0300

    solved test 2 and 3

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,17 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+const number: i32 = 3;
 
 #[cfg(test)]
 mod tests {
+
+use crate::number;    
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(number, 3); //toma dos valores y verifica que sean iguales
+        assert_eq!(false, false);
+        assert_eq!(true, true);
     }
 }
